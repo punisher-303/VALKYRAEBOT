30d6d04d6ca2379e71cfc12b0f57980abda1cb50
punisher-303 <mehafin0@gmail.com>
2024-06-07 21:50:31 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -55,15 +55,9 @@ class Bot(Client):
         logger.info(LOG_MSG.format(me.first_name, date, tame, __repo__, __version__, __license__, __copyright__))
         try: await self.send_message(LOG_CHANNEL, text=LOG_MSG.format(me.first_name, date, tame, __repo__, __version__, __license__, __copyright__), disable_web_page_preview=True)   
         except Exception as e: logger.warning(f"Bot Isn't Able To Send Message To LOG_CHANNEL \n{e}")
-        if WEBHOOK is True:
-            app = web.AppRunner(await web_server())
-            await app.setup()
-            await web.TCPSite(app, "0.0.0.0", 8080).start()
-            logger.info("Web Response Is Running......🕸️")
             
     async def stop(self, *args):
         await super().stop()
-        me = await self.get_me()
         logger.info(f"{me.first_name} is_...  ♻️Restarting...")
 
     async def iter_messages(self, chat_id: Union[int, str], limit: int, offset: int = 0) -> Optional[AsyncGenerator["types.Message", None]]:                       
@@ -79,7 +73,8 @@ class Bot(Client):
 
 
         
-Bot().run()
+app = Bot()
+app.run()
 
 
 
